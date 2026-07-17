e805852aaae4a8a53e489e861f734f141ff4a7fd
Hector-20-glitch <hvilcar@iestlarecoleta.edu.pe>
2026-07-17 12:18:07 -0500

    Add files via upload

---
+++ DWEC_Semana14/js/supabase.js
@@ -0,0 +1,14 @@
+const SUPABASE_URL =
+"https://nzyyruwdnblxpjonccmi.supabase.co";
+
+
+const SUPABASE_KEY =
+"sb_publishable_qqOP5bNYW8wKbhkp0s5XUA_OjUVn1y-";
+
+
+
+const supabaseClient =
+supabase.createClient(
+SUPABASE_URL,
+SUPABASE_KEY
+);
